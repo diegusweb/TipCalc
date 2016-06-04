b981f3f983bf37d37fa188024980c4dc04f2d039
Diego Rueda <diego.rueda2@gmail.com>
2016-06-04 17:24:17 -0400

    add new layout Detail

---
+++ app/src/main/java/com/galileo/diegusweb/tipcalc/activities/TipDetailActivity.java
@@ -0,0 +1,15 @@
+package com.galileo.diegusweb.tipcalc.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.galileo.diegusweb.tipcalc.R;
+
+public class TipDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_detail);
+    }
+}
